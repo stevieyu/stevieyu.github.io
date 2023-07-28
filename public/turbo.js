f4c5e91735b0450bdc55ae3a4e53dddb04920052
StevieYu <yzm629@sina.cn>
2023-07-28 11:05:27 +0000

    up

--- public/turbo.js
+++ public/turbo.js
@@ -14,3 +14,20 @@ addEventListener("turbo:before-visit", (event) => {
 		location.href = event.detail.url
 	}
 })
+
+import OpenReplay from 'https://esm.sh/@openreplay/tracker?bundle'
+import * as Sentry from 'https://esm.sh/@sentry/browser?bundle'
+Sentry.init({
+	dsn: "https://6318dba86435427c87869099519ca6e5@o39671.ingest.sentry.io/4505327000158208",
+	integrations: [
+	  new Sentry.BrowserTracing(),
+	  new Sentry.Replay(),
+	],
+  });
+const tracker = new OpenReplay({
+	projectKey: '7556503659928888',
+	onStart: ({ sessionToken }) => {
+	  Sentry.setTag("openReplaySessionToken", sessionToken);
+	},
+  })
+tracker.start();
