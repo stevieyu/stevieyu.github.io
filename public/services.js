f7933d326f2d65c0ae8a13183fe2d5673b9bda8e
StevieYu <stevieyu@users.noreply.github.com>
2023-08-20 18:22:12 +0800

    Update services.js

--- public/services.js
+++ public/services.js
@@ -14,6 +14,13 @@ addEventListener("turbo:before-visit", (event) => {
 		location.href = event.detail.url
 	}
 })
+addEventListener('popstate', function (e) {
+    if (e.state) {
+	console.log('e.state', e.state)
+	//Turbo.visit('/guides/example/')
+        //load content with ajax
+    }
+});
 
 import OpenReplay from 'https://esm-1251075901.cos-website.ap-hongkong.myqcloud.com/@openreplay/tracker?bundle'
 import * as Sentry from 'https://esm-1251075901.cos-website.ap-hongkong.myqcloud.com/@sentry/browser?bundle'
