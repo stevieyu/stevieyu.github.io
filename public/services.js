fc359343153a20eed8fe5232d2e48d9f591efd3c
StevieYu <stevieyu@users.noreply.github.com>
2023-08-28 21:14:00 +0800

    Update services.js

--- public/services.js
+++ public/services.js
@@ -23,11 +23,19 @@ addEventListener('popstate', (event) => {
 import OpenReplay from 'https://esm.sh/@openreplay/tracker?bundle'
 import * as Sentry from 'https://esm.sh/@sentry/browser?bundle'
 Sentry.init({
-	dsn: "https://6318dba86435427c87869099519ca6e5@o39671.ingest.sentry.io/4505327000158208",
+	dsn: "https://bf0874f6948140d284dd2ac1e20430b5@o39671.ingest.sentry.io/4505606641745920",
 	integrations: [
-	  new Sentry.BrowserTracing(),
+	  new Sentry.BrowserTracing({
+	      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+	      tracePropagationTargets: ["localhost", "https:yourserver.io/api/"],
+	  }),
 	  new Sentry.Replay(),
 	],
+	// Performance Monitoring
+	tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+	// Session Replay
+	replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+	replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
   });
 const tracker = new OpenReplay({
 	projectKey: '7556503659928888',
