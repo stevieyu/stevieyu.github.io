ec24aa1c52c21080dd16aab7b7c83a4f832f8002
Stevie <yzm629@sina.cn>
2023-08-29 19:25:12 +0800

    up

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,65 +3,73 @@ import starlight from '@astrojs/starlight';
 import prefetch from '@astrojs/prefetch';
 import partytown from '@astrojs/partytown';
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+const IS_PROD = process.env.NODE_ENV === 'production';
+
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://stevie.top',
   integrations: [
-    // https://docs.astro.build/en/guides/integrations-guide/sitemap/
-    sitemap(),
-    // https://docs.astro.build/en/guides/integrations-guide/prefetch/
-    // prefetch({
-    //   selector: "a[href^='/']"
-    // }),
-    // https://starlight.astro.build/
-    starlight({
-      title: 'Stevie',
-      // logo: {
-      // light: './src/assets/light-logo.svg',
-      // dark: './src/assets/dark-logo.svg',
-      // src: 'https://avatars.githubusercontent.com/u/8325201?v=4',
-      // replacesTitle: true,
-      // },
-      head: [{
-        tag: 'script',
-        attrs: {
-          src: '/services.js',
-          type: 'module',
-          defer: true
-        }
-      }],
-      locales: {
-        root: {
-          label: '简体中文',
-          lang: 'zh-CN'
-        }
-      },
-      social: {
-        github: 'https://github.com/stevieyu'
-      },
-      sidebar: [{
-        label: 'Guides',
-        items: [
-        // Each item here is one entry in the navigation menu.
-        {
-          label: 'Example Guide',
-          link: '/guides/example/'
-        }]
-      }, {
-        label: 'Reference',
-        autogenerate: {
-          directory: 'reference'
-        }
-      }],
-      lastUpdated: true,
-      editLink: {
-        baseUrl: 'https://github.com/stevieyu/stevieyu.github.io/edit/master'
+  // https://docs.astro.build/en/guides/integrations-guide/sitemap/
+  sitemap(),
+  // https://docs.astro.build/en/guides/integrations-guide/prefetch/
+  // prefetch({
+  //   selector: "a[href^='/']"
+  // }),
+  // https://starlight.astro.build/
+  starlight({
+    title: 'Stevie',
+    // logo: {
+    // light: './src/assets/light-logo.svg',
+    // dark: './src/assets/dark-logo.svg',
+    // src: 'https://avatars.githubusercontent.com/u/8325201?v=4',
+    // replacesTitle: true,
+    // },
+    head: [{
+      tag: 'script',
+      attrs: {
+        src: '/services.js',
+        type: IS_PROD ? 'module' : 'text',
+        defer: true
       }
-    }), 
-    // https://docs.astro.build/en/guides/integrations-guide/partytown/
-    // partytown()
-  ],
+    }],
+    locales: {
+      root: {
+        label: '简体中文',
+        lang: 'zh-CN'
+      }
+    },
+    social: {
+      github: 'https://github.com/stevieyu'
+    },
+    sidebar: [{
+      label: 'Guides',
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: 'Example Guide',
+        link: '/guides/example/'
+      }]
+    }, {
+      label: 'Reference',
+      autogenerate: {
+        directory: 'reference'
+      }
+    }, {
+      label: '例子',
+      autogenerate: {
+        directory: 'examples'
+      }
+    }],
+    lastUpdated: true,
+    editLink: {
+      baseUrl: 'https://github.com/stevieyu/stevieyu.github.io/edit/master'
+    }
+  })
+  // https://docs.astro.build/en/guides/integrations-guide/partytown/
+  // partytown()
+  , tailwind()],
   // https://docs.astro.build/en/guides/assets/#using-sharp
   image: {
     service: {
@@ -70,5 +78,5 @@ export default defineConfig({
   },
   experimental: {
     assets: true
-   }
+  }
 });
