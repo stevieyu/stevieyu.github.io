d3e517c368ae736c693d2eb97c517700e9f89d27
StevieYu <stevieyu@users.noreply.github.com>
2023-09-14 14:33:27 +0800

    auto

--- astro.config.mjs
+++ astro.config.mjs
@@ -50,12 +50,9 @@ export default defineConfig({
       },
       sidebar: [{
         label: 'Guides',
-        items: [
-          // Each item here is one entry in the navigation menu.
-          {
-            label: 'Example Guide',
-            link: '/guides/example/'
-          }]
+        autogenerate: {
+          directory: 'reference'
+        }
       }, {
         label: 'Reference',
         autogenerate: {
