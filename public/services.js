9fc61b963db1470d6a60c709ce5c3658e0e9460f
Stevie <yzm629@sina.cn>
2023-09-24 21:09:04 +0800

    replace cdn

--- public/services.js
+++ public/services.js
@@ -20,27 +20,29 @@ addEventListener('popstate', (event) => {
     }
 });
 
-import OpenReplay from 'https://s.stevie.top/esm.sh/@openreplay/tracker@9.0.8?bundle'
 import * as Sentry from 'https://s.stevie.top/esm.sh/@sentry/browser@7.69.0?bundle'
 Sentry.init({
 	dsn: "https://bf0874f6948140d284dd2ac1e20430b5@o39671.ingest.sentry.io/4505606641745920",
 	integrations: [
-	  new Sentry.BrowserTracing({
-	      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-	    //   tracePropagationTargets: ["localhost", "https:yourserver.io/api/"],
-	  }),
-	  new Sentry.Replay(),
+		new Sentry.BrowserTracing({
+			// Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+			//   tracePropagationTargets: ["localhost", "https:yourserver.io/api/"],
+		}),
+		new Sentry.Replay(),
 	],
 	// Performance Monitoring
 	tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
 	// Session Replay
 	replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
 	replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-  });
+});
+
+import OpenReplay from 'https://s.stevie.top/esm.sh/@openreplay/tracker@9.0.8?bundle'
 const tracker = new OpenReplay({
 	projectKey: '7556503659928888',
 	onStart: ({ sessionToken }) => {
-	  Sentry.setTag("openReplaySessionToken", sessionToken);
+		Sentry.setTag("openReplaySessionToken", sessionToken);
 	},
-  })
+})
 tracker.start();
+
